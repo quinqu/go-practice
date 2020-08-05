e68e9615e494fec6e63ef5e32922375927fc56f0
Quin Quintero <quinq@gravitational.com>
2020-08-05 11:27:23 -0700

    print names of files in which a duplicate line occurs

--- exercise4.go
+++ exercise4.go
@@ -1,6 +1,7 @@
-// Dup2 prints the count and text of lines that appear more than once
-// in the input.  It reads from stdin or from a list of named files.
 package main
+
+// Modify dup2 to print the names of all files
+// in which each duplicated line occurs
 
 import (
 	"bufio"
@@ -12,7 +13,7 @@ func main() {
 	counts := make(map[string]int)
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		countLines(os.Stdin, counts, "")
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -20,21 +21,19 @@ func main() {
 				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 				continue
 			}
-			countLines(f, counts)
+			countLines(f, counts, arg)
 			f.Close()
-		}
-	}
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, counts map[string]int, fileName string) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		counts[input.Text()]++
+		if counts[input.Text()] > 1 {
+			fmt.Println(fileName)
+		}
 	}
-	// NOTE: ignoring potential errors from input.Err()
+
 }
