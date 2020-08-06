07eb04e0331f089ae98b2336e176177bb0e38898
Quin Quintero <quinq@gravitational.com>
2020-08-06 11:06:26 -0700

    exercise 1.7

--- fetch.go
+++ fetch.go
@@ -3,7 +3,7 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
 )
@@ -15,7 +15,11 @@ func main() {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := ioutil.ReadAll(resp.Body)
+
+		src := resp.Body
+		dst := os.Stdout
+		b, err := io.Copy(dst, src)
+
 		resp.Body.Close()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
